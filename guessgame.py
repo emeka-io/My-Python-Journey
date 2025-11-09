e2315e73247376240db2a73c85614b7aebcb7bff
Emeka <theemekamail@gmail.com>
2025-11-09 10:33:18 -0800

    GUESS GAME IN PYTHON
    4 TRIALS, EACH FAILURE GIVES AN HINT, AND REDUCES THE FINAL SCORE

--- guessgame.py
+++ guessgame.py
@@ -1,14 +1,21 @@
 # GUESSING GAME, EACH FAILURE GIVES AN HINT, AND REDUCES THE FINAL SCORE
 hint = [' ', 'The number is even', 'The number is a multiple of 5', 'The number is a multiple of 10', 'The number is a multiple of 7']
-mark = ['100%', '80%', '60%', '40%', '20%']
+mark = ['0%', '100%', '80%', '60%', '40%']
 
 x = 350
 count = 0
 
 while count <= 4:
-    count += 1
     guess = int(input("Enter your guess number: "))
+    count += 1
     if guess == x:
         print(f"You're correct, you aced it in {count} trial(s). Your final score is {mark[count]}")
+        break
+    elif count == 3 and guess != x:
+        print(f"You're incorrect, you've used up {count}/4 trials. Here's a hint for you '{hint[count]}'")
+        print('NOTE: You have 1 trial left')
     else:
         print(f"You're incorrect, you've used up {count}/4 trials. Here's a hint for you '{hint[count]}'")
+    if count == 4 and guess != x:
+        print("Game failed, sorry you've used up all your trials")
+        break
